dc7b6b951ae2426b703ad0b972508791747ce567
Szymon Przybylski <szymon.przybylski@allegro.pl>
2014-04-21 19:18:18 +0200

    deps in tests

--- test/spec/sample.js
+++ test/spec/sample.js
@@ -1,13 +1,14 @@
 define(
     [
-        'application'
+        'vent'
     ],
-    function () {
+    function (vent) {
         'use strict';
 
         describe('Give it some context', function () {
             describe('maybe a bit more context here', function () {
                 it('should run here few assertions', function () {
+                    vent.trigger('event', {});
                     expect('foo').to.not.equal('bar');
                 });
             });
